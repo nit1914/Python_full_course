7bab52d7db917abd5c70808347bdeb5fe23368c0
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-21 20:09:27 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,42 @@
+#String Indexing
+
+# name = "nitesh"
+# print(name[0]) #n
+# print(name[1]) #i
+# print(name[2]) #t
+# print(name[3]) #e
+# print(name[4]) #s
+# print(name[5]) #h
+# print(name[-1]) #h
+# print(name[-2]) #s
+# print(name[-3]) #e
+# print(name[-4]) #t
+# print(name[-5]) #i
+# print(name[-6]) #n
+
+#string slicing
+
+# product = "Laptop pro 2025"
+# print(product[0:6]) #Laptop
+# print(product[7:10]) #pro
+# print(product[-11:-5]) #op pro
+
+# text = "Data Analytics"
+##Extracting first 4 characters
+# print("First 4 characters:", text[0:4]) #Data
+
+##Extracting characters from middle
+# print("Characters from middle:",text[5:14]) #Analytics
+
+##Extracting till end
+# print("Till end:",text[5:]) #Analytics
+
+##Extracting from beginning
+# print("From beginning:",text[:4]) #Data
+
+## Extract last 5 characters
+# print("Last 5 characters:",text[-5:]) #ytics
+
+#skip text
+# print("Skipping characters:",text[0:14:2]) #Dt nltcs
+# print("Reverse :",text[::-1]) #scitynaltA aD
