74be0ebec9e9cd73350de89d4fd888dadf07e345
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-23 22:53:59 +0530

    day8.py

--- day8.py
+++ day8.py
@@ -7,14 +7,19 @@
 #     print("You are not eligible to vote.")
 
 
+
 # # Discount checker
+
 # amount = 1500
 # if amount > 1000:
 #     print("You get a discount!")
 # else:
 #     print("No discount for you.")
 
+
+
 # # If elif else(Multiple conditions)
+
 # marks = int(input("Enter your marks: "))
 # if marks >= 90:
 #     print("Grade: A")
@@ -29,6 +34,7 @@
 
 
 # # Sales performance (Data Analyst Use Case)
+
 # sales = float(input("Enter sales amount: "))
 # if sales >= 10000:
 #     print("Excellent performance!")
@@ -42,6 +48,7 @@
 
 
 # #string comparison Example
+
 # City = "Delhi"
 # if City == "Delhi":
 #     print("City matched")
@@ -55,16 +62,24 @@
 # else:
 #     print("Incorrect password. Please try again.")
 
+
+
 # # Email Validation
+
 # emil = "user@example.com"
 # if "@" in emil and "." in emil:
 #     print("Valid email address.")
 # else:
 #     print("Invalid email address.")
 
+
+
+
 # #Advanced: Missing Data Check
+
 # value = ""
 # if value == "33":
 #     print("Missing data found")
 # else:
 #     print("Data Available")
+
