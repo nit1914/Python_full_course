b100461a80458a0cee779dcd4a1f80b63641703a
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-03-03 10:58:10 +0530

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,41 @@
+# day 16 - sets
+
+# # create set
+# fruits = {"apple", "banana","apple", "cherry"}
+# print(fruits)
+
+# # Add items
+# fruits.add("orange")
+# print(fruits)   
+
+# # Remove items
+# fruits.remove("banana") 
+# # OR
+# fruits.discard("banana")
+# print(fruits)
+
+# # Set Operations
+# a = {1, 2, 3, 4}
+# b = {3, 4, 5, 6}
+# print("Union:", a | b)
+# print("Intersection:", a & b)
+# print("Difference:", a - b)
+# print("Symmetric Difference:", a ^ b)
+
+# # Remove duplicates from a list
+# numbers = [1, 2, 2, 3, 4, 4, 5]
+# unique_numbers = set(numbers)
+# print(unique_numbers)
+
+
+# # Missing values
+# list1 = {"Sql","Excel","Python"}
+# list2 = {"Sql","Python"}
+# missing = list1 - list2
+# print("missing:", missing)
+
+# # Common skills
+# deptA = {"Sql","Excel","Python"}
+# deptB = {"Sql","Python","R"}
+# common = deptA & deptB
+# print("common:", common)
