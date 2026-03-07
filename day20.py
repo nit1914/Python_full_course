19d3ec14f8cdebe1faf825a543fe6bf1a7d94862
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-03-07 12:54:24 +0530

    Add files via upload

--- day20.py
+++ day20.py
@@ -2,26 +2,26 @@
 
 
 # # 1) Reading full file
-# with open("sample.txt","r") as f:
+# with open("day20_samples.txt","r") as f:
 #     print(f.read())
 
 
 # # 2) Reading line by line
-# with open("sample.txt", "r") as f:
+# with open("day20_samples.txt", "r") as f:
 #     for line in f:
 #         print(line.strip().title())
 
 
 
 # # 3) Writing (overwrite)
-# with open("notes.txt", "w") as f:
+# with open("day20_notes.txt", "w") as f:
 #     f.write("Day 20 - File Handling\n")
 #     f.write("Learning read and write.\n")
 
 
 
 # # # 4) Appending
-# with open("notes.txt", "a") as f:
+# with open("day20_notes.txt", "a") as f:
 #     f.write("Appending new line.\n")
 
 
@@ -29,10 +29,10 @@
 
 # # 5) Cleaning data in file
 # cleaned = []
-# with open("sample.txt", "r") as f:
+# with open("day20_samples.txt", "r") as f:
 #     for line in f:
 #         cleaned.append(line.strip().title())  
-# with open("cleaned_output.txt", "w") as f:
+# with open("day20_cleaned_output.txt", "w") as f:
 #     for city in cleaned:
 #         f.write(city + "\n")
 
@@ -40,11 +40,12 @@
 
 # # 6) Counting lines
 # count = 0
-# with open("sample.txt", "r") as f:
+# with open("day20_samples.txt", "r") as f:
 #     for _ in f:
 #         count += 1
 # print("Total Lines:", count)
  
+
 
 
 
